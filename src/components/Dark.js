e868903245391273d5cd4115bc41a5a119ed262a
Reabetswe <140710640+ReaDaCoder@users.noreply.github.com>
2024-08-19 03:37:42 +0200

    fixed dark mode error

--- src/components/Dark.js
+++ src/components/Dark.js
@@ -1,9 +1,56 @@
-  import React from 'react';
-  
-  function Dark(){
+import React, { useEffect } from 'react';
 
-    //const themeBtn = document.querySelector('.toggle');
-   //themeBtn.addEventListener();
+function Dark() {
+  
+  let darkmode = localStorage.getItem('darkmode');
+
+  
+  const enableDark = () => {
+    document.body.classList.add('darkmode');
+    localStorage.setItem('darkmode', 'active');
+  };
+
+  const disableDark = () => {
+    document.body.classList.remove('darkmode');
+    localStorage.setItem('darkmode', null);
+  };
+
+
+  useEffect(() => {
+    if (darkmode === 'active') enableDark();
+
+    const themeSwitch = document.getElementById('theme-switch');
+
+
+    themeSwitch.addEventListener('click', () => {
+      darkmode = localStorage.getItem('darkmode');
+      darkmode !== 'active' ? enableDark() : disableDark();
+    });
+
+    return () => {
+      themeSwitch.removeEventListener('click', () => {});
+    };
+  }, [darkmode]);
+
+  return (
+    <div className="dark-mode">
+      <button className="toggle" id="theme-switch">Switch Theme</button>
+    </div>
+  );
+}
+
+export default Dark;
+
+
+
+  /*
+  const body = document.querySelector('body');
+    const toggle = document.getElementById('toggle');
+    
+    toggle.onClick = function(){
+        toggle.classList.toggle('active');
+        body.classList.toggle('active');
+    }
   
     
     return(
@@ -11,14 +58,48 @@
             <button className="toggle" id="toggle">Switch Theme</button>
         </div>
     )
-  }
-
-  export default Dark
-
-
-  /*const body = document.querySelector('body');
+  
+  
+  
+  
+  
+  const body = document.querySelector('body');
     const toggle = document.getElementById('toggle');
     
     toggle.onClick = function(){
         toggle.classList.toggle('active');
         body.classList.toggle('active');*/
+
+
+        /*import React from 'react';
+  
+  function Dark(){
+
+    let darkmode = localStorage.getItem('darkmode');
+    const themeSwitch = document.getElementById('theme-switch');
+
+    const enableDarK = () =>{
+      document.body.classList.add('darkmode');
+      localStorage.setItem('darkmode', 'active');
+    }
+
+    const disableDark = () => {
+      document.body.classList.remove('darkmode')
+      localStorage.setItem('darkmode', null);
+    }
+
+    if(darkmode === "active") enableDark()
+
+      themeSwitch.addEventListener("click", ()=>{
+        darkmode !== "active" ? enableDark() :disableDark()
+      })
+
+    return(
+      <div className="dark-mode">
+          <button className="toggle" id="theme-switch">Switch Theme</button>
+      </div>
+  )
+
+  }
+
+  export default Dark*/
